f6d9dfc5bea989494f53a570f3d419f46539e86b
wanghuqing <1608751109@qq.com>
2018-05-03 18:59:44 +0800

    .cpp

---
+++ test13-18.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Empolyee
+{
+private:
+	static int a;
+	struct
+	{
+		int number;
+		string name;
+	}data;
+public:
+	Empolyee()
+	{
+		data.number = ++a;
+	}
+	Empolyee(const string& s)
+	{
+		data.number = ++a;
+		data.name = s;
+	}
+	//拷贝构造函数，三五法则；
+	Empolyee(const Empolyee& obj) 
+	{
+		this->data.name = obj.data.name;
+		this->data.number = ++a;
+	}
+	Empolyee& operator=(const Empolyee& obj) //赋值拷贝运算符函数
+	{// 同名的人
+		this->data.name = obj.data.name;
+		return *this;
+	}
+	const string& name()const
+	{
+		return this->data.name;
+	}
+	int number()const
+	{
+		return this->data.number;
+	}
+	~Empolyee()
+	{
+
+	}
+};
+int Empolyee::a = 0;
+
+void _print(const Empolyee& e)
+{
+	cout << e.name() <<" number is: "<<e.number()<< endl;
+}
+int main()
+{
+	Empolyee a("jason");
+	Empolyee b = a , c;
+	c = b;
+	_print(a);
+	_print(b);
+	_print(c);
+	
+	return 0;
+}
